39166f6841e0735a941b7c94311669aa907e18fb
Kim Burgestrand <kim@burgestrand.se>
2009-12-07 10:48:21 +0100

    Initial commit

---
+++ src/laboration3/Main.java
@@ -0,0 +1,18 @@
+/**
+ * Huvudprogrammet.
+ */
+package laboration3;
+
+/**
+ * @author Kim Burgestrand
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+
+}
---
+++ src/laboration3/Performance.java
@@ -0,0 +1,14 @@
+package laboration3;
+
+/**
+ * A model of all seats and their status.
+ *
+ * @author Kim Burgestrand
+ */
+public class Performance {
+    enum Status {
+        Available,
+        Booked,
+        Sold
+    }
+}
