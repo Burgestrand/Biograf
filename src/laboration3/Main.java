487bd90787b1b058a6ab8878c897490b5e002b1d
Kim Burgestrand <kim@burgestrand.se>
2009-12-09 00:16:50 +0100

    Basic layout

--- src/laboration3/Main.java
+++ src/laboration3/Main.java
@@ -5,6 +5,9 @@ package laboration3;
 
 import java.awt.*;
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import sun.awt.HorizBagLayout;
+import sun.awt.VerticalBagLayout;
 
 /**
  * @author Kim Burgestrand
@@ -19,11 +22,36 @@ public class Main {
         frmMain.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frmMain.setPreferredSize(new Dimension(500, 500));
 
-        Performance p = new Performance(10, 30);
-        p.seats(5,26).status = Seat.Status.Sold;
-        PerformanceView pPanel = new PerformanceView(p);
-        frmMain.add(pPanel);
+        // Stolplatser
+        Performance p = new Performance(12, 20);
+        PerformanceView pnlPerformance = new PerformanceView(p);
 
+        // Kontroller
+        //-------------------------------------------------------
+        // Filminfo
+        JPanel pnlFilm = new JPanel();
+        JLabel lblFilm = new JLabel("Mini Ninjas");
+        pnlFilm.setLayout(new GridLayout(2, 1));
+        pnlFilm.add(lblFilm);
+
+        // Knappar
+        JPanel pnlButtons = new JPanel();
+        pnlButtons.setLayout(new GridLayout(1, 2));
+        JButton btnBook = new JButton("Boka");
+        JButton btnBuy  = new JButton("Hämta");
+        pnlButtons.add(btnBook);
+        pnlButtons.add(btnBuy);
+
+        JPanel pnlControls = new JPanel();
+        pnlControls.setLayout(new GridLayout(1, 2));
+        pnlControls.add(pnlFilm);
+        pnlControls.add(pnlButtons);
+        pnlControls.setBorder(new EmptyBorder(10, 10, 10, 10));
+
+        // Lägg till huvudpanelen
+        //---------------------------------------------------------
+        frmMain.add("Center", pnlPerformance);
+        frmMain.add("South", pnlControls);
         frmMain.pack();
         frmMain.setVisible(true);
     }
